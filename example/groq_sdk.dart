83f384bb30ed824645ecfa965ef9ef6fd3eb6b0c
oriventi <adrianus3010@gmail.com>
2024-05-30 23:19:18 +0200

    added example

--- example/groq_sdk.dart
+++ example/groq_sdk.dart
@@ -1,14 +1,19 @@
 import 'package:groq_sdk/groq_sdk.dart';
 
 void main(List<String> arguments) async {
-  final groq = Groq('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');
-  groq.canUseModel(llama3_8b).then((model) {
-    print(model);
-  });
+  final groq = Groq('YOUR_GROQ_API_KEY');
 
+  //Checking model availability
+  if (!await groq.canUseModel(llama3_8b)) {
+    print('Cannot use model');
+    return;
+  }
+
+  //Creating a new chat
   final chat = groq.startNewChat(llama3_8b,
       settings: GroqChatSettings.defaults().copyWith(choicesCount: 2));
 
+  //Listening to chat events
   chat.stream.listen((event) {
     if (event is RequestChatEvent) {
       print('Request sent...');
@@ -18,8 +23,12 @@ void main(List<String> arguments) async {
     }
   });
 
-  final (_, usage) = await chat.sendMessage('Explain LLMs to me please.');
+  //Sending a message which will add new data to the listening stream
+  final (response, usage) =
+      await chat.sendMessage('Explain LLMs to me please.');
 
+  //Printing some information
+  print(response.choices.length);
   print("Total tokens used: ${usage.totalTokens}");
   print("Total time taken: ${usage.totalTime}");
   print("Rate limit information: ${chat.rateLimitInfo.toString()}");
